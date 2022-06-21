bb1f597c36b14240a2c22c3f9a562530783a6ea3
mickael <mickael.cecen@gmail.com>
2022-06-21 17:18:30 +0200

    added logic behind add to cart button

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Layout } from '../component';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
 
 export default MyApp
